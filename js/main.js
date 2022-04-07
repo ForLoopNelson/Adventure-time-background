34c1c2919c9a5fff3f9a4cdb64a85305170793fe
ForLoopNelson <72758221+ForLoopNelson@users.noreply.github.com>
2022-04-07 14:11:13 -0400

    first commit of all files and folders

---
+++ js/main.js
@@ -0,0 +1,57 @@
+document.getElementById('finn').onclick = treeHouse
+document.getElementById('jake').onclick = lady
+document.getElementById('bmo').onclick = bmo
+document.getElementById('pb').onclick = pb
+document.getElementById('marceline').onclick = marceline
+document.getElementById('iceking').onclick = iceKing
+document.getElementById('poohorse').onclick = pooHorse
+
+
+function pooHorse() {
+  document.body.style.backgroundImage = 'url("./img/adventure-time-horse.gif")'
+  document.querySelector("body").style.color = "white"
+  document.body.style.backgroundRepeat = 'repeat'
+  document.body.style.backgroundSize = "auto"
+  
+  
+}
+
+function treeHouse() {
+  document.body.style.backgroundImage = 'url("./img/treehouse.jpg")'
+  document.body.style.backgroundRepeat = 'no-repeat'
+  document.body.style.backgroundSize = 'cover'
+  document.querySelector('body').style.color = 'white'
+}
+
+function lady() {
+  document.body.style.backgroundImage = 'url("./img/lady.png")'
+  document.body.style.backgroundRepeat = "no-repeat"
+  document.body.style.backgroundSize = "cover"
+  document.querySelector('body').style.color = 'white'
+}
+function bmo() {
+  document.body.style.backgroundImage = 'url("./img/treehouse.jpg")'
+  document.body.style.backgroundRepeat = "no-repeat"
+  document.body.style.backgroundSize = "cover"
+  document.querySelector('body').style.color = 'white'
+}
+function pb() {
+  document.body.style.backgroundImage = 'url("./img/PB-castle.jpg")'
+  document.body.style.backgroundRepeat = "no-repeat"
+  document.body.style.backgroundSize = "cover"
+  document.querySelector('body').style.color = 'white'
+}
+function marceline() {
+  document.body.style.backgroundImage = 'url("./img/Marceline\'s_house.png")'
+  document.body.style.backgroundRepeat = "no-repeat"
+  document.body.style.backgroundSize = "cover"
+  document.querySelector('body').style.color = 'white'
+}
+function iceKing() {
+  document.body.style.backgroundImage = 'url("./img/Ice_Kingdom.png")'
+  document.body.style.backgroundRepeat = "no-repeat"
+  document.body.style.backgroundSize = "cover"
+  document.querySelector('body').style.color = 'white'
+}
+
+
